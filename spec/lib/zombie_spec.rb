9f98312b08c085d78336e58a32ed0c4230812ddd
Gajanan Mahajan <gajanan.forums@gmail.com>
2013-10-30 15:48:15 +0530

    1. Added test for no brains 2. Added test for hungry

--- spec/lib/zombie_spec.rb
+++ spec/lib/zombie_spec.rb
@@ -6,4 +6,14 @@ describe Zombie do
 		zombie = Zombie.new
 		zombie.name.should == 'Ash'
 	end
+
+	it 'has no brains' do
+		zombie = Zombie.new
+		zombie.brains.should < 1
+	end
+
+	it 'is hungry' do
+		zombie = Zombie.new
+		zombie.should be_hungry
+	end
 end
